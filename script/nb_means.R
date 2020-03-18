5ebd5e6ff8d0ca9fd2d5b0f9cd9a2f060a2ac5de
zihao12 <wzihao12@gmail.com>
2020-03-18 18:42:05 -0500

    implement a nb_means that works

--- script/nb_means.R
+++ script/nb_means.R
@@ -1,8 +1,9 @@
 ## solve a nb_means problem
-library(CVXR)
+library(stats)
 
 ## model
 ## Y_jk ~ NB(1/phi_jk, 1/(1 + s_k mu_j phi_jk))
+## In the code I use A_jk = 1/Phi_jk instead
 
 ## algorithm
 ## Y_jk ~ Pois(s_k mu_j v_jk)
@@ -16,44 +17,43 @@ library(CVXR)
 
 ## Output
 ## list(mu = mu*, A = A*)
-mle.nb_means.workhorse <- function(Y, s, mu, A, maxiter = 10){
+mle.nb_means.workhorse <- function(Y, s, mu, A, maxiter = 10, verbose = FALSE, 
+                                  control = list(gradient = TRUE, hessian = FALSE)){
   J = nrow(Y)
   K = ncol(Y)
+  Y_rsum = rowSums(Y)
+  progress = c()
+  if(verbose){print("iter 			loglik\n")}
 
   for(iter in 1:maxiter){
-    print(iter)
     #if(iter == 2){browser()}
     ## E-step
     V.pos = (A + Y)/(A + mu %o% s)
     V_log.pos = psigamma(A + Y) - log(A + mu %o% s)
     ## M-step
     ### update mu
-    mu = rowSums(Y)/rowSums(t(s * t(V.pos))) ## TODO: fix the NAN issue
+    mu = Y_rsum/rowSums(t(s * t(V.pos))) ## TODO: fix the NAN issue
+    mu[Y_rsum == 0] = 0
     ### update A
     for(k in 1:K){
-      A[, k] = update_a(a = A[,k], c = V_log.pos[,k] - V.pos[,k])
+      A[, k] = update_a(a = A[,k], c = V_log.pos[,k] - V.pos[,k], 
+                        gradient = control$gradient, hessian = control$hessian)
     }
     ll = loglikelihood.nb_means(Y = Y, s = s, mu = mu, A = A)
-    print(ll)
+    progress <- c(progress, ll)
+    if(verbose){print(sprintf("%d 		%f\n", iter, ll))}
   }
   
-  return(list(mu = mu, A = A))
+  return(list(mu = mu, A = A, progress = progress))
 }
 
 
 ## maximize J(a) = sum( c*a + a*log(a) - lgamma(a) )
 
-update_a <- function(a, c){
-  #browser()
-  n = length(a)
-  x <- Variable(n)
-  x@value = a ## initialize
-  constraints <- list(x >= 0)
-  objective <- Ja.val(x, c)
-  prob <- Problem(Minimize(objective), constraints)
-  result <- solve(prob)
-  a = result$getValue(x)
-  return(a)
+update_a <- function(a, c, gradient, hessian){
+  fn_params = list(c = c, gradient = gradient, hessian = hessian)
+  opt = do.call(nlm, c(list(obj.nb_means, a),fn_params, iterlim = 5))
+  return(opt$estimate)
 }
 
 loglikelihood.nb_means <- function(Y, s, mu, A){
@@ -63,16 +63,22 @@ loglikelihood.nb_means <- function(Y, s, mu, A){
   return(sum(ll))
 }
 
-
-Ja.val <- function(a, c){
-  browser()
-  - sum( c*a + a*log(a) - log(gamma(a@value)) ) ## CVXR doesn't know lgamma?
+obj.nb_means <- function(a, c, gradient = TRUE, hessian =   FALSE){
+  out = Ja.val(a, c)
+  if(gradient){attr(out, 'gradient') <- Ja.grad(a, c)}
+  if(hessian){attr(out, 'hessian') <- Ja.hess(a)}
+  return(out)
 }
 
-# Ja.grad <- function(a, c){
-#   - (  log(a) - psigamma(a, deriv = 0) + c + 1  )
-# }
 
-# Ja.hess <- function(a){
-#   - ( 1/a - psigamma(a, deriv = 1) )
-# }
+Ja.val <- function(a, c){
+  - sum( c*a + a*log(a) - lgamma(a) ) 
+}
+
+Ja.grad <- function(a, c){
+  - (  log(a) - psigamma(a, deriv = 0) + c + 1  )
+}
+
+Ja.hess <- function(a){
+  diag(- ( 1/a - psigamma(a, deriv = 1) ))
+}
--- script/test_nb_means.R
+++ script/test_nb_means.R
@@ -3,8 +3,8 @@ rm(list = ls())
 source("nb_means.R")
 set.seed(123)
 
-K = 30
-p = 99
+K = 3
+p = 999
 eps = 1e-3
 signal = 10
 s = replicate(K, 1)
@@ -16,9 +16,10 @@ Phi[((2*p/3)+1):p,3] = 1 + signal * runif(p/3)
 A = 1/Phi
 
 ## simulate data from the model
-
 V = matrix(rgamma(n = p*K, shape = A, rate = A), nrow = p)
 Theta = (mu %o% s) * V
 Y = matrix(rpois(n = p * K, lambda = Theta), nrow = p)
 
-fit = mle.nb_means.workhorse(Y, s, mu, A, maxiter = 10)
+fit = mle.nb_means.workhorse(Y, s, mu, A, maxiter = 100, verbose = TRUE,
+                            control = list(gradient = TRUE, hessian = FALSE))
+plot(fit$progress)
