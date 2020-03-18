c5553bf140b6759df953f63bce03557a58caf99a
zihao12 <wzihao12@gmail.com>
2020-03-18 01:00:41 -0500

    start working on nb means

--- script/nb_means.R
+++ script/nb_means.R
@@ -1,35 +1,78 @@
 ## solve a nb_means problem
-
+library(CVXR)
 
 ## model
 ## Y_jk ~ NB(1/phi_jk, 1/(1 + s_k mu_j phi_jk))
+
+## algorithm
+## Y_jk ~ Pois(s_k mu_j v_jk)
+## v_jk ~ Gamma(a_jk, a_jk), a_jk = 1/phi_jk
 
 ## Input
 ## Y: count matrix of dimension [J, K]
 ## s: non-negative vector of length K
 ## mu: initiliazation of mean vector, length J
-## phi: initialization of variance matrix, dimension [J, K]
+## A: initialization of 1/variance matrix, dimension [J, K]
 
 ## Output
-## list(mu = mu*, phi = phi*)
-mle.nb_means.workhorse <- function(Y, s, mu, Phi, maxiter = 10){
+## list(mu = mu*, A = A*)
+mle.nb_means.workhorse <- function(Y, s, mu, A, maxiter = 10){
   J = nrow(Y)
   K = ncol(Y)
 
   for(iter in 1:maxiter){
-    mu = update_mu(Y = Y, s = s, mu = mu, Phi = Phi)
+    print(iter)
+    #if(iter == 2){browser()}
+    ## E-step
+    V.pos = (A + Y)/(A + mu %o% s)
+    V_log.pos = psigamma(A + Y) - log(A + mu %o% s)
+    ## M-step
+    ### update mu
+    mu = rowSums(Y)/rowSums(t(s * t(V.pos))) ## TODO: fix the NAN issue
+    ### update A
     for(k in 1:K){
-      Phi[, k] = update_phi(y = Y[,k], s = s[k], mu = mu, phi = Phi[,k])
+      A[, k] = update_a(a = A[,k], c = V_log.pos[,k] - V.pos[,k])
     }
+    ll = loglikelihood.nb_means(Y = Y, s = s, mu = mu, A = A)
+    print(ll)
   }
-
-  return(list(mu = mu, Phi = Phi))
+  
+  return(list(mu = mu, A = A))
 }
 
-update_mu <- function(Y,s, mu, Phi){
-  ## TODO
+
+## maximize J(a) = sum( c*a + a*log(a) - lgamma(a) )
+
+update_a <- function(a, c){
+  #browser()
+  n = length(a)
+  x <- Variable(n)
+  x@value = a ## initialize
+  constraints <- list(x >= 0)
+  objective <- Ja.val(x, c)
+  prob <- Problem(Minimize(objective), constraints)
+  result <- solve(prob)
+  a = result$getValue(x)
+  return(a)
 }
 
-update_phi <- function(y, s, mu, phi){
-  ## TODO
+loglikelihood.nb_means <- function(Y, s, mu, A){
+  
+  Phi = 1 / A
+  ll = lgamma(Y + A) - lgamma(A) - (Y  + A) * log(1 + (mu %o% s)*Phi ) + Y * (mu + log(Phi))
+  return(sum(ll))
 }
+
+
+Ja.val <- function(a, c){
+  browser()
+  - sum( c*a + a*log(a) - log(gamma(a@value)) ) ## CVXR doesn't know lgamma?
+}
+
+# Ja.grad <- function(a, c){
+#   - (  log(a) - psigamma(a, deriv = 0) + c + 1  )
+# }
+
+# Ja.hess <- function(a){
+#   - ( 1/a - psigamma(a, deriv = 1) )
+# }
---
+++ script/test_nb_means.R
@@ -0,0 +1,24 @@
+##
+rm(list = ls())
+source("nb_means.R")
+set.seed(123)
+
+K = 30
+p = 99
+eps = 1e-3
+signal = 10
+s = replicate(K, 1)
+mu = runif(p) 
+Phi = matrix(eps * runif(p * K), nrow = p, ncol = K)
+Phi[1:(p/3),1] = 1 + signal * runif(p/3)
+Phi[((p/3)+1):(2*p/3), 2] = 1 + signal * runif(p/3)
+Phi[((2*p/3)+1):p,3] = 1 + signal * runif(p/3)
+A = 1/Phi
+
+## simulate data from the model
+
+V = matrix(rgamma(n = p*K, shape = A, rate = A), nrow = p)
+Theta = (mu %o% s) * V
+Y = matrix(rpois(n = p * K, lambda = Theta), nrow = p)
+
+fit = mle.nb_means.workhorse(Y, s, mu, A, maxiter = 10)
